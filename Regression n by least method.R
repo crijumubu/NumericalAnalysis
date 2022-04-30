a578da7e2ccb3671a2469acd81287dc6c629d55a
Cristian Julian Muñoz Buenahora <cristian.munoz.2020@upb.edu.co>
2022-04-30 18:41:32 -0500

    Updating the file with the latest modifications

--- Regression n by least method.R
+++ Regression n by least method.R
@@ -1,98 +1,168 @@
 ### INITIAL INFORMATION FOR LABORATORY SOLUTION
 
-
 vectorx <- c(-1,3,8,16,17,22,24)
+#vectorx <- c(-2,-1,0,1,2)
+#vectory <- c(3,0,2,4,4)
 vectory <- c(-5,2,7,9,18,20,22)
-n <- 4 ### FUNCTION ORDER
-contx <- 0
-conty <- 1
-row_length <- n + 1                                        
-column_length <- n + 2
-matrix <- matrix(0:0, nrow <- row_length , ncol <- column_length)
+### FUNCTION THAT DOES THE ENTIRE REGRESION PROCESS
 
-
-### NEW VARIABLES DEFINITION
-
-
-extension <- n * 2
-sumx <- c(1:extension)
-sumy <- 0
-sumxy <- c(1:extension)
-sumx[1] <- n
-
-
-### CALCULATION AND CREATION OF THE MATRIX WITH THE GIVEN VECTORS
-
-
-for (i in 1:extension){
-  resultx <- 0
-  resultxy <- 0
-  sumy <- 0
-  for (j in 1:length(vectorx)){
-    resultx <- resultx + vectorx[j] ** (i)
-    sumy <- sumy + vectory[j]
-    resultxy <- resultxy + (vectorx[j] ** (i-1) * vectory[j])
+regresion_calculator <- function(n){
+  
+  ### VARIABLES AND VECTORS DEFINITION
+  
+  column_length <- n + 2
+  contx <- 0
+  conty <- 1
+  determination_coefficient <- 0
+  extension <- n * 2
+  row_length <- n + 1     
+  sumx <- c(1:extension)
+  sumx[1] <- length(vectorx)
+  sumxy <- c(1:extension)
+  y_average <- 0
+  
+  matrix <- matrix(0:0, nrow <- row_length , ncol <- column_length)
+  
+  ### CALCULATION AND CREATION OF THE MATRIX WITH THE GIVEN VECTORS
+  
+  for (i in 1:extension){
+    resultx <- 0
+    resultxy <- 0
+    y_average <- 0
+    x_average <- 0
+    for (j in 1:length(vectorx)){
+      resultx <- resultx + vectorx[j] ** (i)
+      y_average <- y_average + vectory[j]
+      resultxy <- resultxy + (vectorx[j] ** (i-1) * vectory[j])
+    }
+    sumxy[i] <- resultxy
+    sumx[i+1] <- resultx
   }
-  sumxy[i] <- resultxy
-  sumx[i+1] <- resultx
-}
-
-for (i in 1:row_length){
-  for (j in 1:column_length){
-    if (j != column_length){
-      matrix[i,j] <- sumx[j+(i-1)]
-    }else{
-      matrix[i,j] <- sumxy[i]
+  y_average <- y_average / length(vectory)
+  
+  for (i in 1:row_length){
+    for (j in 1:column_length){
+      if (j != column_length){
+        matrix[i,j] <- sumx[j+(i-1)]
+      }else{
+        matrix[i,j] <- sumxy[i]
+      }
     }
   }
-}
-
-
-cat("This is the matrix generated from the values of x and y defined in the vectors:\n\n")
-print(matrix)
-
-
-### GAUSS METHOD
-
-
-for (i in 1:(n)){
-  for (j in (i+1):row_length){
-    scalar <- matrix[j,i]/matrix[i,i]
-    for (k in 1:column_length){
-      matrix[j,k] <- matrix[j,k] - matrix[i,k] * scalar
-    }
-  }
-}
-
-cat("\nThis is the resulting matrix after performing the Gauss method:\n\n")
-print(matrix)
-
-
-### GAUSS-JORDAN METHOD
-
-
-for (i in 1:(n+1)){
-  for (j in 1:(i-1)){
-    if (i != 1){
+  
+  ### GAUSS METHOD
+  
+  for (i in 1:(n)){
+    for (j in (i+1):row_length){
       scalar <- matrix[j,i]/matrix[i,i]
       for (k in 1:column_length){
         matrix[j,k] <- matrix[j,k] - matrix[i,k] * scalar
       }
     }
   }
+  
+  ### GAUSS-JORDAN METHOD
+  
+  for (i in 1:(n+1)){
+    for (j in 1:(i-1)){
+      if (i != 1){
+        scalar <- matrix[j,i]/matrix[i,i]
+        for (k in 1:column_length){
+          matrix[j,k] <- matrix[j,k] - matrix[i,k] * scalar
+        }
+      }
+    }
+  }
+  
+  
+  for (i in 1:row_length){
+    scalar <- matrix[i,i]
+    for (j in i:column_length){
+      matrix[i,j] <- matrix[i,j] / scalar
+    }
+  }
+  
+  ### SAVE THE RESULTS IN A NEW VECTOR
+  
+  resultVector <- c()
+  string_result_vector <- ''
+  for (i in 1:row_length){
+    resultVector[i] <- matrix[(row_length - i) + 1,column_length]
+    string_result_vector <- paste(string_result_vector, round(resultVector[i], digits = 5))
+  }
+  
+  ### CALCULATE Y ESTIMATED
+  
+  yestimated <- c()
+  
+  for (i in 1:length(vectory)){
+    yestimated[i] <- 0
+    for (j in 1:length(resultVector)){
+      yestimated[i] <- yestimated[i] + (resultVector[j]*(vectorx[i])**((length(resultVector) - j)))
+    }
+  }
+  
+  ### CALCULATE DETERMINATION COEFFICIENT
+  
+  y_minus_yaverage_squared <- 0
+  y_minus_yestimated_squared <- 0
+  
+  for (i in 1:length(vectory)){
+    y_minus_yaverage_squared <- y_minus_yaverage_squared + ((vectory[i]-y_average)**2)
+    y_minus_yestimated_squared <- y_minus_yestimated_squared + ((vectory[i] - yestimated[i])**2)
+  }
+  
+  determination_coefficient <- ((y_minus_yaverage_squared - y_minus_yestimated_squared) / y_minus_yaverage_squared) * 100
+  
+  ### IF DETERMINATION COEFFICIENT VALUE IS NAN, THEN CHANGE IT TO 0 FOR PRACTICAL PURPOSES
+  
+  if (is.nan(determination_coefficient)){
+    determination_coefficient <- 0
+  }
+  
+  ### RETURN THE DETERMINATION COEFFICIENT TOGETHER WITH THE COEFFICIENTS OF THE CALCULATED EQUATION
+  
+  result_function <- c(determination_coefficient, string_result_vector)
+  
+  return(result_function)
+  
 }
 
 
-for (i in 1:row_length){
-  scalar <- matrix[i,i]
-  for (j in i:column_length){
-    matrix[i,j] <- matrix[i,j] / scalar
+### SOLUTION TO THE PROPOSED POINTS
+
+
+### 1) FIND A REGRESSION OF GRADE N BY LEAST SQUARES THAT GENERATES AT LEAST AN R^2 OF 70%
+
+n <- 2 
+while (TRUE){
+  resultVector <- regresion_calculator(n)
+  if (resultVector[1] < 70){
+    n <- n + 1
+  }else{
+    cat("THE REGRESSION OF GRADE", n, "GENERATES AT LEAST AN R^2 OF 70%\n")
+    cat("\tCOEFFICIENTS OF THE EQUATION ->", resultVector[2], "\n\n")
+    break
   }
 }
 
+### 2) FIND THE BEST REGRESSION OF GRADE N BY LEAST SQUARES WHERE N RANGES FROM 2 TO 15
 
-cat("\nFinally, to find out the result, we apply Gauss-Jordan method, this is the resulting matrix after performing this:\n\n")
-print(matrix)
+all_results_vector <- c()
+for (i in 2:15){
+  all_results_vector[i] <- regresion_calculator(i)[1]
+}
 
+best_r2 <- as.numeric(all_results_vector[2])
+best_r2_position <- 2
+for (i in 3:15){
+  if (best_r2 < as.numeric(all_results_vector[i])){
+    best_r2 <- as.numeric(all_results_vector[i])
+    best_r2_position <- i
+  }
+}
 
-### STUDENT: CRISTIAN JULIAN MUNOZ BUENAHORA - 000430876
+cat("THE BEST REGRESSION IS OF ", best_r2_position, "TH GRADE WITH AN R^2 VALUE OF ", best_r2, "\n", sep = "")
+cat('\tCOEFFICIENTS OF THE EQUATION ->', regresion_calculator(best_r2_position)[2])
+
+### STUDENT: CRISTIAN JULIAN MUNOZ BUENAHORA - 000430876 
